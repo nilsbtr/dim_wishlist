5bb09c9e28f315385b05e73febca282371ec64b8
PunchedYou <nibot04@gmail.com>
2022-12-07 11:37:22 +0100

    updated database

--- src/define.py
+++ src/define.py
@@ -35,7 +35,6 @@ def def_register(path):
 weapons = def_dictionary('assets/dictonary/dic_weapons.txt')
 suppl = def_dictionary('assets/dictonary/dic_suppl.txt')
 traits = def_dictionary('assets/dictonary/dic_traits.txt')
-enhanced = def_dictionary('assets/dictonary/dic_enhanced.txt')
 exotics = def_dictionary('assets/dictonary/dic_exotics.txt')
 
 craftable = def_register('assets/craftable.txt')
